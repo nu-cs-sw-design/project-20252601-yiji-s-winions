55fbb6cd251b3ef0bec88e17c6968550dffa1b21
unknown <paulkim2025@u.northwestern.edu>
2025-11-23 14:15:14 -0600

    Renamed CompositeUserRepository to UserRepository. Now build succeeds

--- src/User.java
+++ src/User.java
@@ -6,9 +6,9 @@ public class User {
     private final String userId;
     private final String username;
     private String passwordHash;
-    private final ConcreteUserRepository userRepository; // Direct dependency on concrete repo
+    private final UserRepository userRepository; // Direct dependency on concrete repo
 
-    public User(String username, String passwordHash, ConcreteUserRepository userRepository) {
+    public User(String username, String passwordHash, UserRepository userRepository) {
         this.userId = UUID.randomUUID().toString();
         this.username = username;
         this.passwordHash = passwordHash;
